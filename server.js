0202f9bc8c11d2f7a2573cee66069d5ec4d930cd
Analee Guerra <aguerra1508@gmail.com>
2020-10-24 13:37:41 -0500

    files & folders

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 // Dependencies
-var express = require("express");
-var db = require("./models");
-var mysql = require("mysql");
-require('dotenv').config()
+const express = require("express");
+const db = require("./models");
 
 // Sets up the Express App
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
+require("dotenv").config();
 
 // Starts the server to begin listening
 db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 });
