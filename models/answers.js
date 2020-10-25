d9024f8276b74b7bcc632bcb989781272123218a
Laura Rendon <lrendon1220@gmail.com>
2020-10-25 16:07:15 -0500

    updates

--- models/answers.js
+++ models/answers.js
@@ -1,10 +1,12 @@
-module.exports = function(sequelize, DataTypes) {
-    var Answers = sequelize.define("Answers", {
-        answer: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }
-    })
+module.exports = function (sequelize, DataTypes) {
+        var Answers = sequelize.define("Answers", {
+                answer: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    validate: {
+                        len: [1]
+                    }
+                })
 
-    return Answers;
-}
+            return Answers;
+        }
