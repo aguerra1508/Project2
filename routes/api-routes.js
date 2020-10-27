200bee294f69e11287408ff9339f81e6d4b7a062
Analee Guerra <aguerra1508@gmail.com>
2020-10-26 21:33:06 -0500

    added passport, passport local for email and password for user authentication

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,76 @@
 const db = require("../models");
-const Question = require("../models/questions");
+
 
 module.exports = function(app) {
-    app.get("/api/:question", function(req,res) {
-        db.findAll({}).then(function(dbQuestion){
-            res.json(dbQuestion);
+    
+    app.get("/api/posts/", function(req,res) {
+        db.User.findAll({}).then(function(dbUser) {
+            console.log(dbUser)
+            res.json(dbUser);
         });
     });
 
-    app.get("/api/:answer", function(req,res){
-        db.findOne({}).then(function(dbAnswer){
-            res.json(dbanswer);
+    app.get("/api/posts/user", function(req,res) {
+        db.User.findAll({
+            where: {
+                name: req.params.name,
+                score: req.params.score
+            }
+        }).then(function(dbUser) {
+            console.log(dbUser)
+            res.json(dbUser);
+        });
+    });
+    
+    app.get("/api/posts/:id", function(req,res) {
+        db.User.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbUser) {
+            console.log(dbUser)
+            res.json(dbUser);
+        });
+    });
+    
+    app.post("/api/posts", function(req,res){
+        console.log(req.body);
+        db.User.create({
+            name: req.body.name,
+            email: req.body.email,
+        }).then(function(dbUser){
+            console.log(dbUser)
+            res.json(dbUser);
+        });
+    });
+    
+    app.post("/api/posts/question", function(req,res){
+        console.log(req.body);
+        db.Questions.create({
+            question: req.body.question,
+        }).then(function(dbPost){
+            res.json(dbPost);
+        });
+    });
+
+    app.post("/api/posts/answers", function(req,res){
+        console.log(req.body);
+        db.Answers.create({
+            answer: req.body.answer
+        }).then(function(dbAnswers){
+            console.log(dbAnswers);
+            res.json(dbAnswers);
+        });
+    });
+    
+    app.delete("/api/posts/:id", function(req,res) {
+        db.User.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbUser) {
+            console.log(dbUser)
+            res.json(dbUser);
         });
     });
 
