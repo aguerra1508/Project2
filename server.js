3e59abb88dcba40d0be8ad099b121430e3d82bbd
Analee Guerra <aguerra1508@gmail.com>
2020-10-31 13:40:21 -0500

    sign up page for passport authenication

--- server.js
+++ server.js
@@ -2,12 +2,21 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 require("dotenv").config();
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 app.subscribe(express.json());
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Require models
 const db = require("./models");
