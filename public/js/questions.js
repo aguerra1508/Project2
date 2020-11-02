f80f6e14aeb00607f2f252d196ce399fe30f017c
Keita Suzuki <ksuzuki417@gmail.com>
2020-11-01 19:38:50 -0600

    adding analee's work

--- public/js/questions.js
+++ public/js/questions.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
     var question = [""];
     var answer = 
 
+    app.get("/:id", function(req, res) {
+        connection.query("SELECT * FROM Questions where id = ? ORDER BY RAND() LIMIT 5", [req.params.id], function(err, data) { 
+            if (err) {
+                return res.status(500).end();
+            }
+        }
+    });
 
 
 
